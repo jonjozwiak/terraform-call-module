25b9ba7b789173e7ab86577dda13668aba284cfa
Jon Jozwiak <jonjozwiak@users.noreply.github.com>
2023-03-17 12:47:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 
 module "hello-world" {
   #source = "git@github.com/jonjozwiak/hello-world-module-test.git//?ref=main"
-  source = "git::https://github.com/jonjozwiak/hello-world-module-test.git//?ref=main
+  source = "git::https://github.com/jonjozwiak/terraform-hello-world-module.git//?ref=main"
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
